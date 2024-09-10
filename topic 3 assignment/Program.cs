bad6df7598164b3e1e9681835f58fcc60069489f
339749939 <339749939@2260I-L29-015>
2024-09-10 09:28:57 -0400

    Add project files.

---
+++ topic 3 assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace topic_3_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int room_number = 29;
+            double price = 2.99;
+            string name = "Computer science!";
+            Console.WriteLine("");
+            Console.WriteLine("this is room " + room_number);
+            Console.WriteLine("The price is " + price);
+            Console.WriteLine("I am learning " + name);
+            string myname = "Horse Tranqullizer";
+            int gradyear = 2026;
+            Console.WriteLine("My name is " + myname);
+            Console.WriteLine("I graduate in " + gradyear);
+            double cRad = 3;
+            double area = (cRad * cRad) * 3.14;
+            Console.WriteLine(area);
+            
+
+
+
+
+
+        }
+    }
+}
